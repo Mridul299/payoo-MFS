49056fdf4ef70efdedc7c81ad488a0c3f859f559
Mridul299 <midulislam820@gmail.com>
2024-09-19 12:02:20 +0600

    aded a new page

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('amar sonar bangla ami tomay vlo basi');
+document.getElementById('btn-login').addEventListener('click',function(event){
+    event.preventDefault();
+    const phoneNumber = document.getElementById("phone-number").value;
+    const pinNumber = document.getElementById("pin-number").value;
+    if(pinNumber === '1234' && phoneNumber === '01786104945'){
+        window.location.href="/home.html"
+    }
+    else{
+        const messeage = "wrong phone number or wrong pin number";
+        const p = document.getElementById("wrong-pin");
+        p.innerText = messeage;
+        
+    };
+   
+
+})
