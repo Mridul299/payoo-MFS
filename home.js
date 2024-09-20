d0a4225587451eb2000ef196433ed401b6ece035
Mridul299 <midulislam820@gmail.com>
2024-09-21 01:09:55 +0600

    fifty persent done

--- home.js
+++ home.js
@@ -1,12 +1,58 @@
 document.getElementById('add-money').addEventListener('click',function(){
     document.getElementById('addmoney-form').classList.remove('hidden');
     document.getElementById('cashout-form').classList.add('hidden');
+
+
+
 });
 
+document.getElementById("btn-addmoney").addEventListener('click',function(event){
+    event.preventDefault();
+    const pininput = document.getElementById("add-pin-number").value;
+    const amountinput = document.getElementById("add-amount-number").value;
+    const addbalance = parseFloat(amountinput);
+    const balance = document.getElementById("av-balance").innerText;
+    const balanceNumber = parseFloat(balance);
+
+    if(pininput === '1234' && amountinput >= '0' ){
+        const newbalance = balanceNumber + addbalance;
+        document.getElementById("av-balance").innerText = newbalance;
+    }
+    else{
+        alert('please input something valid');
+    }
+    
+});
 
 
 
 document.getElementById('cash-out').addEventListener('click',function(){
     document.getElementById('cashout-form').classList.remove('hidden');
     document.getElementById('addmoney-form').classList.add('hidden');
+});
+
+document.getElementById("btn-cashout").addEventListener('click',function(event){
+    event.preventDefault();
+    const pininput = document.getElementById("pin-number").value;
+    const amountinput = document.getElementById("cashout-amount-number").value;
+    const addbalance = parseFloat(amountinput);
+    const balance = document.getElementById("av-balance").innerText;
+    const balanceNumber = parseFloat(balance);
+
+    if(pininput === '1234'){
+        const newbalance = balanceNumber - addbalance;
+        document.getElementById("av-balance").innerText = newbalance;
+    }
+    else{
+        alert('please input something valid');
+    };
+
+    if(addbalance <= balanceNumber){
+        const newbalance = balanceNumber - addbalance;
+        document.getElementById("av-balance").innerText = newbalance;
+    }
+    else{
+        alert('insuficent balance');
+    };
+    
 });
