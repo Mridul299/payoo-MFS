d2dcc980d50cf18f4d1f412f26d956d8ffee1c3e
Mridul299 <midulislam820@gmail.com>
2024-09-21 01:58:21 +0600

    final test

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ document.getElementById('btn-login').addEventListener('click',function(event){
     const phoneNumber = document.getElementById("phone-number").value;
     const pinNumber = document.getElementById("pin-number").value;
     if(pinNumber === '1234' && phoneNumber === '01786104945'){
-        window.location.href="/home.html"
+        window.location.href="./home.html";
     }
     else{
         const messeage = "wrong phone number or wrong pin number";
